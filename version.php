aa6510baa5d6dfb124385bd5a9b66ea990017ca3
Eduardo Kraus <kraus@eduardokraus.com>
2026-03-06 15:16:30 -0300

    V 1.1.6

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026030500;
-$plugin->release   = '1.1.5';
+$plugin->version = 2026030600;
+$plugin->release   = '1.1.6';
 $plugin->requires  = 2023042400;
 $plugin->component = 'local_gimidashboard';
 $plugin->maturity  = MATURITY_STABLE;
